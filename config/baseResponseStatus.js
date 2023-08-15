00b5ed767f529d7c8e51d0cb04dbf5538173b4d6
seungwanRyu01 <seungwan6255@gmail.com>
2023-08-15 19:13:10 +0900

    docs : boardView response add

--- config/baseResponseStatus.js
+++ config/baseResponseStatus.js
@@ -5,6 +5,8 @@ module.exports = {
     REGISTER_SUCCESS : { "isSuccess": true, "code": 1002, "message":"회원가입 성공" },
     LOGIN_SUCCESS : { "isSuccess": true, "code": 1003, "message":"로그인 성공" },
     BOARD_INSERT_SUCCESS : { "isSuccess": true, "code": 1004, "message":"게시글 작성 성공" },
+    BOARD_PAGING_VIEW_SUCCESS : { "isSuccess": true, "code": 1005, "message":"게시글 목록 조회 성공" },
+    BOARD_ELEMENT_VIEW_SUCCESS : { "isSuccess": true, "code": 1006, "message":"특정 게시글 조회 성공" },
 
     // Common
     TOKEN_EMPTY : { "isSuccess": false, "code": 2000, "message":"JWT 토큰을 입력해주세요." },
@@ -18,6 +20,9 @@ module.exports = {
     
     BOARD_TITLE_EMPTY : { "isSuccess": false, "code": 2005, "message": "제목을 입력해주세요." },
     BOARD_DESCRIPTION_EMPTY : { "isSuccess": false, "code": 2006, "message": "내용을 입력해주세요." },
+
+    BOARD_PAGING_EMPTY : { "isSuccess": false, "code": 2007, "message": "페이지 위치 또는 페이지 사이즈를 입력해주세요." },
+    BOARD_INDEX_EMPTY : { "isSuccess": false, "code": 2008, "message": "게시글 인덱스를 입력해주세요." },
 
     // Response error
     SIGNUP_REDUNDANT_EMAIL : { "isSuccess": false, "code": 3001, "message":"이미 가입된 이메일 주소입니다. 다른 이메일을 입력하여 주세요." },
