ce17b8087fd8a9590bdabc388307bcdc4d5148e1
seungwanRyu01 <seungwan6255@gmail.com>
2023-08-15 23:58:34 +0900

    docs : 1007, 2009 response code add

--- config/baseResponseStatus.js
+++ config/baseResponseStatus.js
@@ -7,6 +7,7 @@ module.exports = {
     BOARD_INSERT_SUCCESS : { "isSuccess": true, "code": 1004, "message":"게시글 작성 성공" },
     BOARD_PAGING_VIEW_SUCCESS : { "isSuccess": true, "code": 1005, "message":"게시글 목록 조회 성공" },
     BOARD_ELEMENT_VIEW_SUCCESS : { "isSuccess": true, "code": 1006, "message":"특정 게시글 조회 성공" },
+    BOARD_EDIT_SUCCESS : { "isSuccess": true, "code": 1007, "message":"게시글 수정 성공" },
 
     // Common
     TOKEN_EMPTY : { "isSuccess": false, "code": 2000, "message":"JWT 토큰을 입력해주세요." },
@@ -23,6 +24,8 @@ module.exports = {
 
     BOARD_PAGING_EMPTY : { "isSuccess": false, "code": 2007, "message": "페이지 위치 또는 페이지 사이즈를 입력해주세요." },
     BOARD_INDEX_EMPTY : { "isSuccess": false, "code": 2008, "message": "게시글 인덱스를 입력해주세요." },
+
+    BOARD_USERIDX_NOT_MATCH : { "isSuccess": false, "code": 2009, "message": "게시글을 작성한 유저가 아닙니다." },
 
     // Response error
     SIGNUP_REDUNDANT_EMAIL : { "isSuccess": false, "code": 3001, "message":"이미 가입된 이메일 주소입니다. 다른 이메일을 입력하여 주세요." },
