40eccde2774914ece9f1bd57f3bf402f07424400
seungwanRyu01 <seungwan6255@gmail.com>
2023-08-16 10:55:29 +0900

    initial : ecosystem.config.js -> add

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+        {
+            name: 'wanted-api',
+            script: 'yarn node index.js',
+            cwd: '/home/ubuntu/dev/wanted-pre-onboarding-backend',
+            instances: 1,
+            autorestart: true,
+            watch: true
+        },
+    ],
+};
