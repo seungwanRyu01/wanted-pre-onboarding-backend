352a1792efa1bc657138e45305aff241afb5b17a
seungwanRyu01 <seungwan6255@gmail.com>
2023-08-16 11:08:01 +0900

    fix : ecosystem.config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,10 @@ module.exports = {
     apps: [
         {
             name: 'wanted-api',
-            script: 'yarn node index.js',
+            script: 'sudo yarn node index.js',
             cwd: '/home/ubuntu/dev/wanted-pre-onboarding-backend',
             instances: 1,
-            autorestart: false,
+            autorestart: true,
             watch: true
         },
     ],
